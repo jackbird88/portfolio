ed955e58a601c5fd2a49508b0236b2757fce9069
jackbird88 <jack.a.bird88@gmail.com>
2024-11-14 18:20:12 -0500

    Set up basic about page

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/About.module.css';
+
+const About = () => {
+  return (
+    <section className={styles.aboutContainer}>
+      <h2>About Me</h2>
+      <p>
+        I’m a dedicated software engineer with a passion for building scalable web applications and innovative user experiences. I love problem-solving and bringing ideas to life through code.
+      </p>
+    </section>
+  );
+};
+
+export default About;
